0de9d867741c42e6b31a1b67a15f9de19ab0fff8
Adam Ellis <adam.ellis.programmer@gmail.com>
2025-10-07 19:18:39 +0100

    Initial commit

---
+++ src/components/BookingSlot.jsx
@@ -0,0 +1,183 @@
+'use client'
+import React, { useState, useEffect } from 'react'
+const BookingSlot = ({ setBookignDetails, bookignDetails }) => {
+  const [bookingSlots, setBookingSlots] = useState([
+    { dates: [''], places: 10, month: '', year: '', show: true },
+  ])
+
+  useEffect(() => {
+    return () => {}
+  }, [])
+
+  // prettier-ignore
+  const months = ['January', 'Febuary', 'March', 'April', 'May', 'June','July', 'August', 'September', 'October', 'November', 'December']
+
+  const years = ['2025', '2026', '2027', '2028', '2029']
+  //
+  const updateDateInSlot = (slotIndex, dateIndex, value) => {
+    const [year, month, day] = value.split('-')
+    const date = `${day}/${month}/${year}`
+
+    const updated = [...bookingSlots]
+    // console.log(updated[slotIndex])
+    updated[slotIndex].dates[dateIndex] = date
+    setBookingSlots(updated)
+    console.log(bookingSlots)
+  }
+
+  const addSlot = () => {
+    console.log('added slot...')
+    setBookingSlots([
+      ...bookingSlots,
+      { dates: [''], places: 10, month: '', year: '', show: true },
+    ])
+  }
+
+  const addDateToSlot = (slotIndex) => {
+    console.log(slotIndex)
+    const updated = [...bookingSlots]
+    updated[slotIndex].dates.push('')
+    setBookingSlots(updated)
+  }
+
+  const deleteDate = (slotIndex, dateIndex) => {
+    const updated = [...bookingSlots]
+
+    updated[slotIndex].dates = updated[slotIndex].dates.filter(
+      (_, i) => i !== dateIndex
+    )
+    setBookingSlots(updated)
+  }
+
+  const deleteBookingSlot = (slotIndex) => {
+    console.log(slotIndex)
+
+    let updated = [...bookingSlots]
+    // const obj = updated[slotIndex]
+    // updated = updated.filter((item, i) => i !== slotIndex)
+    // console.log(updated)
+    // console.log(obj.dates)
+    // setBookingSlots(updated)
+    //  or do it this way
+    setBookingSlots(bookingSlots.filter((_, i) => i !== slotIndex))
+  }
+  return (
+    <div className=' mb-15'>
+      <h3 className='text-center text-4xl capitalize'>Booking Dates entery </h3>
+      <section>
+        <div className='max-w-[1200px] mx-auto p-10'>
+          <ul>
+            {bookingSlots.map((booking, slotIndex) => {
+              return (
+                <li
+                  key={slotIndex}
+                  className='mb-5 border-b pb-10 border-dashed relative'
+                >
+                  {bookingSlots.length > 1 && (
+                    <button
+                      onClick={() => deleteBookingSlot(slotIndex)}
+                      className='absolute right-0 bg-rose-500 p-2 rounded-2xl'
+                    >
+                      delete
+                    </button>
+                  )}
+                  <div className=''>
+                    <p className='text-2xl capitalize'>bookable dates</p>
+                    <p className='text-2xl capitalize'>select dates month</p>
+                    <select
+                      className='border w-[150px] p-2 text-lg mx-1 rounded'
+                      name=''
+                      id=''
+                    >
+                      {months.map((month, i) => {
+                        return (
+                          <option key={i} value={month}>
+                            {month}
+                          </option>
+                        )
+                      })}
+                    </select>
+                    <select
+                      className='border w-[150px] p-2 text-lg mx-1 rounded'
+                      name=''
+                      id=''
+                    >
+                      {years.map((year, i) => {
+                        return (
+                          <option key={i} value={year}>
+                            {year}
+                          </option>
+                        )
+                      })}
+                    </select>
+                  </div>
+
+                  <div className='mt-5'>
+                    <p className='text-2xl capitalize'>
+                      Number of people per one booking
+                    </p>
+                    <select className='border p-1 w-[210px]' name='' id=''>
+                      {[...Array(10)].map((_, i) => (
+                        <option key={i} value={i + 1}>
+                          {i + 1}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                  {/* dates  */}
+                  <div className='mt-4'>
+                    <p className='text-2xl capitalize'>Available Dates</p>
+
+                    {booking.dates.map((item, dateIndex) => {
+                      return (
+                        <div key={dateIndex} className='mb-3 flex h-full'>
+                          <input
+                            onChange={(e) =>
+                              updateDateInSlot(
+                                slotIndex,
+                                dateIndex,
+                                e.target.value
+                              )
+                            }
+                            type='date'
+                            className='border w-full p-2'
+                          />
+                          {booking.dates.length > 1 && (
+                            <button
+                              onClick={() => deleteDate(slotIndex, dateIndex)}
+                              className='w-[50px] ml-10 bg-red-500 rounded-full'
+                            >
+                              X
+                            </button>
+                          )}
+                        </div>
+                      )
+                    })}
+                  </div>
+                  <div>
+                    <button
+                      onClick={() => addDateToSlot(slotIndex)}
+                      className='bg-orange-500 p-2 rounded-2xl'
+                    >
+                      add date
+                    </button>
+                  </div>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+        <div className='flex justify-center'>
+          <button
+            onClick={addSlot}
+            className='bg-orange-500 capitalize p-3 rounded-2xl'
+          >
+            add block dates
+          </button>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default BookingSlot
---
+++ src/components/ItineraryDays.jsx
@@ -0,0 +1,94 @@
+'use client'
+import Image from 'next/image'
+import { useState, useEffect } from 'react'
+
+export default function AddItinerary() {
+  const [itineraryDays, setItineraryDays] = useState([
+    { dayNumber: 1, dayTitle: '', dayDescription: '' },
+  ])
+
+  useEffect(() => {
+    return () => {}
+  }, [])
+
+  const addDay = () => {
+    console.log('adding day....')
+    setItineraryDays([
+      ...itineraryDays,
+      { dayNumber: itineraryDays.length + 1, dayTitle: '', dayDescription: '' },
+    ])
+  }
+
+  const removeDay = (index) => {
+    console.log('removing day ....')
+    setItineraryDays(itineraryDays.filter((item, i) => i !== index))
+  }
+  console.log(itineraryDays)
+
+  const updateDay = (index, field, value) => {
+    const updated = [...itineraryDays] // copy
+    // updated[index] is an object === {dayNumber: 1, dayTitle: '', dayDescription: ''}
+    updated[index] = { ...updated[index], [field]: value }
+    setItineraryDays(updated)
+    //
+    //
+    //
+    // console.log(updated[index])
+    // console.log('updated : ', updated)
+  }
+
+  return (
+    <div className=''>
+      <h3 className='text-center text-4xl'>Trip Itiniary </h3>
+      <ul className=''>
+        {itineraryDays.map((item, i) => {
+          return (
+            <li key={i} className=' mb-5 border-b border-dashed pb-'>
+              <p className=' p-3  capitalize'>day number: {item.dayNumber}</p>
+              <div>
+                <input
+                  className='border-b w-full text-lg p-2 mb-2'
+                  type='text'
+                  value={item.dayTitle}
+                  onChange={(e) => updateDay(i, 'dayTitle', e.target.value)}
+                  placeholder='Enter Day Name'
+                />
+                <textarea
+                  className='border w-full rounded h-[100px] p-5'
+                  name=''
+                  id=''
+                  placeholder='Day Description'
+                  onChange={(e) =>
+                    updateDay(i, 'dayDescription', e.target.value)
+                  }
+                ></textarea>
+              </div>
+
+              {/* day description: {item.dayDescription} */}
+
+              <div className='flex justify-end'>
+                <button
+                  onClick={() => removeDay(i)}
+                  className='bg-rose-500 p-2 rounded-2xl'
+                >
+                  remove
+                </button>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
+      {/* itinerary button */}
+      <div className='mt-8 flex justify-center'>
+        <button
+          onClick={addDay}
+          className='bg-blue-500 p-7 text-2xl rounded-2xl'
+        >
+          add +
+        </button>
+      </div>
+
+      {/* ====================================================== */}
+    </div>
+  )
+}
